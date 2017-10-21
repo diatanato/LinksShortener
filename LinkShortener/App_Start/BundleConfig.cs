0dd40a36afae5178a367f0e4a8759e5a0858e37f
diatanato <diatanato@yandex.ru>
2017-10-21 23:05:07 +0300

    разбавим все это вторым ангуляром

---
+++ LinkShortener/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Optimization;
+
+namespace LinkShortener
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/scripts/bootstrap.js",
+                      "~/scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/content/css").Include(
+                      "~/content/bootstrap.css",
+                      "~/content/site.css"));
+        }
+    }
+}
---
+++ LinkShortener/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app",
+    template: `<div></div>`
+})
+export class AppComponent { }
